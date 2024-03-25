79e02ba174334708f7dc088779f8ba11c231b3f3
MattiaBologna <mattiabologna01@gmail.com>
2024-03-25 14:42:15 +0100

    Add: Vue cdn link and messages

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('test');
+
+
+        const { createApp } = Vue
+        
+        createApp({
+            data() {
+                return {
+                    message: 'Testo diverso da: Hello Vue!',
+                    anotherMessage: 'Test'
+                }
+    }
+  }).mount('#app')
